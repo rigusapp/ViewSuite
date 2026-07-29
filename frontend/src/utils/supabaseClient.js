cf66e4aa45fb7bcc3ce509d175ac8f5d62110848
Agus <media.agussiahaan@gmail.com>
2026-07-29 15:58:08 +0700

    fix: safe initialization for supabase client

--- frontend/src/utils/supabaseClient.js
+++ frontend/src/utils/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zufoxendmitbbuvyxzed.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1Zm94ZW5kbWl0YmJ1dnl4emVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMDg2MjAsImV4cCI6MjEwMDg4NDYyMH0.bfz6AMLiIkUuSjHmeMZ8Sds5Be0J5j_rRjx-3IdFwd0';
+
+if (!import.meta.env.VITE_SUPABASE_URL || !import.meta.env.VITE_SUPABASE_ANON_KEY) {
+  console.error('⚠️ Supabase URL / Anon Key belum dikonfigurasi di Environment Variables!');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
